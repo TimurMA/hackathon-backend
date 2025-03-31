df6f697bc0c29e9caded8e2dacd82837399b7d19
Тимур <timur.minyazeff@gmail.com>
2025-03-31 11:41:38 +0500

    some refactor (maybe)

--- app/NLP document module/DocumentReader.py
+++ app/NLP document module/DocumentReader.py
@@ -1,6 +1,6 @@
-import spacy
 from spacy_layout import spaCyLayout
 from spacy.matcher import Matcher
+from spacy import load
 from docling_core.types.doc.document import DoclingDocument
 from app.common_dictionaries import competence_list
 # Зависимости, которых может и не быть
@@ -12,7 +12,7 @@ from app.common_dictionaries import competence_list
 class DocumentReader:
     def __init__(self):
         #загрузка модели
-        self.nlp = spaCyLayout(spacy.load("ru_core_news_sm"))
+        self.nlp = spaCyLayout(аload("ru_core_news_sm"))
     # Метод для чтения информации с документа, возвращает два словаря, первый компетенции, второй доп. информация
     def read_document(self, document: DoclingDocument | bytes):
         # Загрузка документа в spaCy
@@ -28,7 +28,7 @@ class DocumentReader:
             "EMAIL":[],
             "URL":[]
         }
-        matcher = Matcher(self.nlp.vocab)
+        matcher = Matcher(self.nlp.nlp.vocab)
         # регулярки для нахождения доп данных
         email_patterns = [{"TEXT": {"REGEX": "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"}}]
         phone_patterns = [
@@ -38,9 +38,9 @@ class DocumentReader:
         ]
         url_patterns=[{"TEXT": {"REGEX": "(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])"}}]
         matcher.add("PHONE_NUMBER", phone_patterns)
-        matcher.add("EMAIL", email_patterns)
-        matcher.add("URL", url_patterns)
+        matcher.add("EMAIL", [email_patterns])
+        matcher.add("URL", [url_patterns])
         matches = matcher(doc)
         for match_id, start, end in matches:
-            info[self.nlp.vocab.strings[match_id]].append(doc[start:end].text)
+            info[self.nlp.nlp.vocab.strings[match_id]].append(doc[start:end].text)
         return [competences,info]
