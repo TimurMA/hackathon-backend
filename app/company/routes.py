8089077556802cdc97f30d1527e77daa406d320f
Тимур <timur.minyazeff@gmail.com>
2025-04-12 17:11:05 +0500

    added docker compose dev with dockerfile, fix creating and updating and routing

--- app/company/routes.py
+++ app/company/routes.py
@@ -14,15 +14,13 @@ company_router = APIRouter(
     tags=["Company"]
 )
 
+@company_router.get("/all")
+async def get_all_companies_async(session: Session, company_filter: CompanyFilter = FilterDepends(CompanyFilter)) -> Sequence[CompanyPublic]:
+    return await get_all_companies(session, company_filter)
 
 @company_router.get("/{company_id}")
 async def get_company_by_id_async(company_id: str, session: Session) -> CompanyPublic:
     return await get_company_by_id(company_id, session)
-
-
-@company_router.get("/all")
-async def get_all_companies_async(session: Session, company_filter: CompanyFilter = FilterDepends(CompanyFilter)) -> Sequence[CompanyPublic]:
-    return await get_all_companies(session, company_filter)
 
 
 @company_router.post("/add")
@@ -35,7 +33,7 @@ async def update_company_async(company_id: str, company: CompanySave, session: S
     return await update_company(company_id, company, session)
 
 
-@company_router.delete("delete/{company_id}")
+@company_router.delete("/delete/{company_id}")
 async def delete_company_async(company_id: str, session: Session):
     return await delete_company(company_id, session)
 
