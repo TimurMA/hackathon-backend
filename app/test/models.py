4ecc18036ad26b56d972b2313d4e5e5c16b6bfdf
Тимур <timur.minyazeff@gmail.com>
2025-04-13 17:02:49 +0500

    fix competence adding, update test module models.py, update docker files

--- app/test/models.py
+++ app/test/models.py
@@ -1,13 +1,53 @@
+from sqlalchemy import TIMESTAMP
 from sqlmodel import SQLModel, Field, Relationship
 from enum import Enum
 from uuid import uuid4, UUID
+from datetime import datetime, timezone
 
+from app.company.models import Company
 from app.user.models import User
+from app.vacancy.models import VacancyCompetence
 
 
 class QuestionType(str, Enum):
     SINGLE = 'single'
     MULTIPLE = 'multiple'
+    TEXT = 'text'
+
+class VacancyCompetenceTest(SQLModel, table=True):
+    __tablename__ = "vacancy_competence_tests"
+
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+
+    test_id: UUID = Field(foreign_key="tests.id")
+    vacancy_id: UUID = Field(foreign_key="vacancies.id")
+    competence_id: UUID = Field(foreign_key="competencies.id")
+
+class TestBase(SQLModel):
+    name: str = Field(max_length=255)
+    test_time: int = Field(default=0)
+
+class Test(TestBase, table=True):
+    __tablename__ = "tests"
+
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    company_id = Field(foreign_key="companies.id")
+
+    company: Company = Relationship(sa_relationship_kwargs={"lazy": "selectin"})
+    vacancy_competencies: list[VacancyCompetence] = Relationship(sa_relationship_kwargs={"lazy": "selectin"}, link_model=VacancyCompetenceTest)
+
+class TestResultBase(SQLModel):
+    started_at: datetime = Field(sa_column=TIMESTAMP(True))
+    finished_at: datetime = Field(default=datetime.now(timezone.utc), sa_column=TIMESTAMP(True))
+
+class TestResult(TestResultBase, table=True):
+    __tablename__ = "test_results"
+    id: UUID = Field(foreign_key="tests.id", primary_key=True)
+    user_id: UUID = Field(foreign_key="users.id")
+
+    user: User = Relationship(sa_relationship_kwargs={ "lazy": "selectin" })
+    user_choices: list["UserChoice"] = Relationship(back_populates="test_result", sa_relationship_kwargs={ "lazy": "selectin"})
+
 
 class QuestionBase(SQLModel):
     q_type: QuestionType = Field(default=QuestionType.SINGLE)
@@ -23,10 +63,8 @@ class UserChoiceBase(SQLModel):
 class UserChoice(UserChoiceBase, table=True):
     __tablename__ = "user_choices"
 
-    question_id: UUID = Field(foreign_key="questions.id")
+    test_result_id: UUID = Field(foreign_key="test_results.id")
     answer_id: UUID = Field(foreign_key="answers.id")
-    user_id: UUID = Field(foreign_key="users.id")
 
-    user: User = Relationship(sa_relationship_kwargs={ "lazy": "selectin"})
-
+    test_result: TestResult = Relationship(back_populates="user_choices", sa_relationship_kwargs={"lazy": "noload"})
 
